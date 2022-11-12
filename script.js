5bfd8bc6b1c1f91f253b27b315a192b38c51320d
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2022-11-13 00:55:28 +0530

    Version 0

---
+++ script.js
@@ -0,0 +1,7 @@
+a = Math.floor(Math.random()*(999-100+1)+100)
+b = Math.floor(Math.random()*(999-100+1)+100)
+
+document.getElementById("num1").innerHTML = a;
+document.getElementById("num2").innerHTML = b;
+console.log(a)
+console.log(b)

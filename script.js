22102ca0400f102f8471295c6f18d1217f96909e
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2022-11-15 00:31:59 +0530

    Version 1

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// Generating question for Multiplying 2 3-Digit Numbers
 a = Math.floor(Math.random()*(999-100+1)+100)
 b = Math.floor(Math.random()*(999-100+1)+100)
 
 document.getElementById("num1").innerHTML = a;
 document.getElementById("num2").innerHTML = b;
-console.log(a)
-console.log(b)
+//console.log(a)
+//console.log(b)
+
+// Generating question for finding Square-root of a number from 1 to 100
+c = Math.floor(Math.random()*(100-1+1)+1)
+document.getElementById("num3").innerHTML = c*c;
+//console.log(c)
+
+// Generating question for finding Cube-root of a number from 1 to 100
+d = Math.floor(Math.random()*(100-1+1)+1)
+document.getElementById("num4").innerHTML = d*d*d;
+//console.log(d)

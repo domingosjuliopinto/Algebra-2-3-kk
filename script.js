a8f5eccb8b1fd9097b5cc51ef4416b4f72a9c07d
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2022-11-25 12:26:53 +0530

    Version 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function randomNumber(maxi, mini) {
     return Math.floor(Math.random()*(maxi-mini+1)+mini);
 }
-
+/*
 // Generating question for Multiplying 2 3-Digit Numbers
 a = randomNumber(999,100)
 b = randomNumber(999,100)
@@ -9,10 +9,17 @@ b = randomNumber(999,100)
 document.getElementById("num1").innerHTML = a;
 document.getElementById("num2").innerHTML = b;
 
+console.log(a);
+console.log(b);
+*/
 // Generating question for finding Square-root of a number from 1 to 100
 c = randomNumber(100,1)
 document.getElementById("num3").innerHTML = c*c;
 
+console.log(c);
+
 // Generating question for finding Cube-root of a number from 1 to 100
 d = randomNumber(100,1)
 document.getElementById("num4").innerHTML = d*d*d;
+
+console.log(d);
